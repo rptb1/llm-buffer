c83e83b2228d2222789931cfe744074da8fbe593
Richard Brooksby <rptb1+git@pobox.com>
2025-09-16 08:37:58 +0100

    Licensing under GPL 3.

--- llm-buffer.el
+++ llm-buffer.el
@@ -13,6 +13,21 @@
 ;;; maintain the object in a buffer local variable and update it,
 ;;; rather than generating a new one.
 
+;;; Copyright 2025 Richard Brooksby.
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License as
+;;; published by the Free Software Foundation; either version 3 of the
+;;; License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
 (require 'cl-lib)
 (require 'llm) ; See <https://github.com/ahyatt/llm>.
 (require 'llm-openai)
